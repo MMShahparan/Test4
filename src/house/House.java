fa657105359299f03069d4e2a2ac9fa6d17ca21d
MMShahparan <sparan27@gmail.com>
2019-02-22 20:31:27 -0500

    d

---
+++ src/house/House.java
@@ -0,0 +1,23 @@
+package house;
+
+public class House {
+	
+//	 int houseAddress;
+//	String  houseName;
+	
+//	static int houseAddress=5;
+//	String  houseName="Bangladesh";
+	
+	public void watchGame() {
+		System.out.println("This is our house, we are watching soccer");
+		
+	}
+	
+	public int addition(int number1, int number2) {
+		int total = number1 + number2;
+		return total;
+		
+	}
+	
+
+}
---
+++ src/house/TestHouse.java
@@ -0,0 +1,33 @@
+package house;
+
+public class TestHouse {
+
+	public static void main(String[] args) {
+	
+		House check1 = new House(); // object creation
+		check1.watchGame();
+		
+		Office check2 = new Office();
+		check2.officeLocation();
+		
+		
+		
+//		int t=check1.houseAddress;
+//		System.out.println(t);
+		
+
+//		House yourHouse = new House();
+//		yourHouse.watchGame();
+//		
+//		int result = myHouse.addition(10,15);
+//		System.out.println(result);
+//		
+//		Office pnt = new Office();
+//		pnt.officeLocation();
+		
+		
+		
+			
+	}
+
+}
